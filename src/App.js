db62162a76bb418ad7bb2bce0ee1fe6fbdb65308
ssiperko <s.siperko@gmail.com>
2018-02-26 07:26:37 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className='app'>
-          <h1 className="hello">Hello!</h1>
+          <h1 className="hello">Hello, May!</h1>
           <h1>Welcome to your awesome demo site.</h1>
       </div>
     );
