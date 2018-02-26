0655fe503fdd82e882b743f3f78a448ad0d37964
ssiperko <s.siperko@gmail.com>
2018-02-26 07:23:03 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+          <h1 className="hello">Hello!</h1>
+          <h1>Welcome to your awesome demo site.</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
