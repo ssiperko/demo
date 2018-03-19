f3049cd0a443171e595e9b25572f1db5fe34104a
ssiperko <s.siperko@gmail.com>
2018-03-19 14:58:31 -0400

    updates

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,31 +1,33 @@
 import React from 'react';
 import {Head} from './Head';
-import {Profile} from './EmployeeProfile';
 
 export class About extends React.Component{
   render(){
     return(
       <div>
           <Head />
-          <h1>About us</h1>
-          <div className='aboutContainer'>
-              <div className="photoContainer">
-                  <img className="coverImage" src="https://i.pinimg.com/originals/65/09/0a/65090aac124066b4c9bd3459a5c9fe6e.jpg" alt='workers' />
+          <div className='photoContainer'>
+              <h1 className='aboutCoverText'>“Every informed person needs to know about Bitcoin because it might be one of the world’s most important developments.” -Leon Luow, Nobel Prize Nominee</h1>
+              <img className='coverImage' src="https://gcn.com/~/media/GIG/GCN/Redesign/Articles/2017/April/blockchain2.png" alt='niagra falls' />
+          </div>
+              <h1 className="pageHeadline">About us</h1>
+          <div className="aboutContentContainer">
+              <div>
+                  <h1 className="who">Who we are</h1>
+                  <p className="aboutContent">
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.""Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                  </p>
               </div>
-          </div>
-          <div>
-          <h1>--Our Mission--</h1>
-          <p>
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.""Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-          </p>
-          </div>
-          <div>
-              <Profile />
-              <Profile />
-              <Profile />
-              <Profile />
-              <Profile />
-              <Profile />
+              <div>
+                  <h1 className="mission">Our Mission</h1>
+                  <p className="aboutContent">
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+                  </p>
+                  <h1 className="mission">What we do</h1>
+                  <p className="aboutContent">
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+                  </p>
+              </div>
           </div>
       </div>
     );
