11ee6c3750c62042a5c6a1545824f44f2904bc56
ssiperko <s.siperko@gmail.com>
2018-03-30 11:05:02 -0400

    style

--- src/Components/Foot.js
+++ src/Components/Foot.js
@@ -5,9 +5,16 @@ export class Foot extends React.Component{
       return(
         <div>
             <div className='foot'>
-            <h1>Contact:</h1>
-                <h2 className='footItems'>Telephone: 407 555 5566</h2>
-                <h2><a className='footItems' href="mailto:steven@steven-siperko.space">E-mail: xyz@abc.com</a></h2>
+                <div className='footItems'>
+                    <h1>BlockPro</h1>
+                </div>
+                <div className='footItems footLogo'>
+                    <h1 className="footLogo">Company Logo</h1>
+                </div>
+                <div className='footItems'>
+                    <i className="fa fa-phone footIcon"></i>
+                    <a href="mailto:blockprocapital@gmail.com"><i className="fa fa-envelope footIcon"></i></a>
+                </div>
             </div>
             <div className='subFoot'>
                 <h5 className="subFootItems">designed and coded by Steven Luke Siperko </h5> <h5 className="subFootItems"> copyright BlockPro Inc.</h5>
