49a2d3641277ad38afa767750b151baa703d4b3d
ssiperko <s.siperko@gmail.com>
2018-04-13 14:10:29 -0400

    storage

--- src/Components/NewsSlide.js
+++ src/Components/NewsSlide.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 export class NewsSlide extends React.Component{
-    render(){
+  render(){
       return(
-        <div className="newsSlideContainer">
-            <img className="newsLogo" src={this.props.logo} />
+        <div style={{backgroundColor: this.props.color}} className="newsSlideContainer">
+            <div>
+                <h5 className="newsLogo">published by</h5>
+                <img className="newsLogo" src={this.props.logo} />
+            </div>
             <div className="newsHeadlineContainer">
                 <h1>{this.props.headline}</h1>
                 <a href={this.props.link} target="_blank"><button className="newsLink">view article</button></a>
